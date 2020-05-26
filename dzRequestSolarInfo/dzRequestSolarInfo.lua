2725be00f0fe8438141751149d1f49ec4a1c0e90
Richard Dols <github@dols.nu>
2020-05-26 22:44:04 +0200

    Refactored and functional day graphs

--- dzRequestSolarInfo/dzRequestSolarInfo.lua
+++ dzRequestSolarInfo/dzRequestSolarInfo.lua
@@ -1,10 +1,13 @@
 --------------------------------------------------------------------------------
---Config
+-- Config
 local enableDebugPrint = true
 local isDecimalComma = true -- true for 1.000,00 :: false for 1,000.00 
 local username = "your@email.com"
 local password = "password"
 local siteID = "0000000"
+--------------------------------------------------------------------------------
+-- Upvalues
+local timezoneOffset = 0
 --------------------------------------------------------------------------------
 local function LogDebug(domoticz, log)
     if enableDebugPrint then
@@ -25,22 +28,29 @@ end
 --------------------------------------------------------------------------------
 local function AddReportData(json, component)
     local id = tostring(component.id)
-    component.DayEnergy = 0
+    component.DayWh = 0
     if json.reportersData[id] then
-        component.DayEnergy = json.reportersData[id].unscaledEnergy or 0
+        component.DayWh = json.reportersData[id].unscaledEnergy or 0
     end
 end
 --------------------------------------------------------------------------------
 local function AddReportInfo(json, component)
     local id = tostring(component.id)
     data = json.reportersInfo[id]
-    component.CurrentPower = 0
+    component.Watt = 0
     if data then
-        --component.LastMeasurement = data.lastMeasurement --Not updated by website, useless
+        if data.lastMeasurement then
+            -- Not always updated although there is new data
+            -- unixtimestamp in mSec and LOCAL time
+            component.LastMeasurement = (data.lastMeasurement / 1000) + timezoneOffset
+        else
+            component.LastMeasurement = 0
+        end
+        
         component.Name = data.name
         for k, v in pairs(data.localizedMeasurements or {}) do
             if string.match(k, "%[W%]") then
-                component.CurrentPower = MakeCleanNumber(v)
+                component.Watt = MakeCleanNumber(v)
             end
         end
     end
@@ -107,13 +117,59 @@ local function RequestSolardEdge(domoticz)
 end
 --------------------------------------------------------------------------------
 local function UpdateDevice(domoticz, deviceType, device)
-    LogDebug(domoticz, string.format('%s "%s" Energy = %0.2f Wh   Current Power = %0.2f W', deviceType, device.Name, device.DayEnergy, device.CurrentPower))
-    if domoticz.utils.deviceExists(device.Name) then
-        --local newTotalValue = domoticz.devices(device.Name).WhTotal - domoticz.devices(device.Name).WhToday + device.DayEnergy
-        local newTotalValue = device.DayEnergy
-        domoticz.devices(device.Name).updateElectricity(device.CurrentPower, newTotalValue)
-    else
+
+    if not domoticz.utils.deviceExists(device.Name) then
         LogDebug(domoticz, string.format('to monitor create an dummy device type="Electric (Instant+Counter) name="%s"', device.Name))
+        return
+    end
+
+    local lastData = domoticz.data.LastData[device.Name]
+    if lastData == nil then
+        domoticz.data.LastData[device.Name] = {Watt = device.Watt, DayWh=device.DayWh}
+        return
+    end
+    
+    local lastWatt = lastData.Watt or 0
+    local lastDayWh = lastData.DayWh or 0
+    
+    if device.DayWh == lastDayWh and device.Watt == lastWatt then
+        return -- No new data
+    end
+    
+    if device.DayWh < lastDayWh  then
+        lastDayWh = 0 -- Most likely a new day
+    end
+    
+    local dzDevice = domoticz.devices(device.Name)
+    local addWh = device.DayWh - lastDayWh
+    local newWh = dzDevice.WhTotal + addWh
+    
+    if device.Name == "Panel 1.0.1" or device.Name == "Panel 1.0.4" or device.Name == "Inverter 1" then
+        --LogDebug(domoticz, string.format('----- %s "%s" ----------------------------------', deviceType, device.Name))
+        --LogDebug(domoticz, string.format('Last Update: %s', os.date('%Y-%m-%d %H:%M:%S', device.LastMeasurement)))
+        
+        --LogDebug(domoticz, string.format('device.DayWh=%0.3f  lastDayWh=%0.3f  device.Watt=%0.3f  lastWatt=%0.3f', device.DayWh, lastDayWh, device.Watt, lastWatt))
+        --LogDebug(domoticz, string.format('device.DayWh - lastDayWh = addWh => %0.3f - %0.3f = %0.3f', device.DayWh, lastDayWh, addWh))
+        --LogDebug(domoticz, string.format('old.WhTotal + addWh = newWh => %0.3f + %0.3f = %0.3f', dzDevice.WhTotal, addWh, newWh))
+
+        --LogDebug(domoticz, string.format('OldTotal=%0.3f   DomoticzToday=%0.3f   lastDayWh=%0.3f   PanelToday=%0.3f   newWh=%0.3f', dzDevice.WhTotal, dzDevice.WhToday, lastDayWh, device.DayWh, newWh))
+        
+        LogDebug(domoticz, string.format('%s "%s" Today = %0.2f Wh   Now = %0.2f W', deviceType, device.Name, device.DayWh, device.Watt))
+    end
+    dzDevice.updateElectricity(device.Watt, newWh)
+
+    domoticz.data.LastData[device.Name] = {Watt = device.Watt, DayWh=device.DayWh}
+end
+--[[----------------------------------------------------------------------------
+Timestamps in the json are local times, not UTC. 
+Here we calculate the timezoneOffset upvalue. 
+It has a known bug. Arround the daylight saving switch it has an error of 1 hour
+--]]
+local function CalculateTimezone()
+    timezoneOffset = os.time{year=1970, month=1, day=1, hour=0}
+    local localTime = os.date("*t", os.time())
+    if localTime.isdst then
+        timezoneOffset = timezoneOffset - 3600
     end
 end
 --------------------------------------------------------------------------------
@@ -137,16 +193,19 @@ return
 --------------------------------------------------------------------------------
     data = 
 	{
+	    LastData = { initial = {} }
     },
 --------------------------------------------------------------------------------
     execute = function(domoticz, item)
 --------------------------------------------------------------------------------
         if (item.isTimer) then
-            LogDebug(domoticz, "-=[ Start HTTP Request]=======================================")
+            LogDebug(domoticz, "-=[ Start HTTP Request ]======================================")
             RequestSolardEdge(domoticz)
         elseif (item.isHTTPResponse) then
             LogDebug(domoticz, "-=[ Start HTTP Response ]=====================================")
             if (item.ok) then
+                CalculateTimezone()
+                
                 local info = { Inverters = {}, Strings = {}, Optimizers = {} }
                 for _, child in pairs(item.json.logicalTree.children) do
                     local inverter = GetInverterData(item.json, info, child)
