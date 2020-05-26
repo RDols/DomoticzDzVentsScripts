05d8e93d7dfd8bdef2a3150c336b7e6f99c1a4c7
Richard Dols <github@dols.nu>
2020-05-26 22:53:23 +0200

    refactored, added Energy Consumed

--- dzPollSolarEdge/dzPollSolarEdge.lua
+++ dzPollSolarEdge/dzPollSolarEdge.lua
@@ -7,33 +7,8 @@ local script = "/home/dols/effe/sunspec-status"
 -- Also check trigger
 -- check device names
 --------------------------------------------------------------------------------
--- Enum of the csv fields
-local timestamp = 1
-local status = 2
-local ac_power = 3
-local dc_power = 4
-local total_production = 5
-local ac_voltage = 6
-local ac_current = 7
-local dc_voltage = 8
-local dc_current = 9
-local temperature = 10
-local exported_energy_m1 = 11
-local imported_energy_m1 = 12
-local exported_energy_m2 = 13
-local imported_energy_m2 = 14
---------------------------------------------------------------------------------
 -- Status to string
-local statusStringMap= {}
-statusStringMap[0] = "Unknown"
-statusStringMap[1] = "OFF"
-statusStringMap[2] = "SLEEPING"
-statusStringMap[3] = "STARTING"
-statusStringMap[4] = "ON (MPPT)"
-statusStringMap[5] = "THROTTLED"
-statusStringMap[6] = "SHUTTING DOWN"
-statusStringMap[7] = "FAULT"
-statusStringMap[8] = "STANDBY"
+local statusStringMap = {"OFF", "SLEEPING", "STARTING", "ON (MPPT)", "THROTTLED", "SHUTTING DOWN", "FAULT", "STANDBY"}
 --------------------------------------------------------------------------------
 local function LogDebug(domoticz, log)
     if enableDebugPrint then
@@ -43,6 +18,15 @@ end
 --------------------------------------------------------------------------------
 local function LogError(domoticz, log)
     print(string.format('dzPollSolarEdge : %s', log))
+end
+--------------------------------------------------------------------------------
+local function GetDevice(domoticz, deviceName, dzDevice)
+    if not domoticz.utils.deviceExists(deviceName) then
+        return false
+    end
+    
+    dzDevice.meter = domoticz.devices(deviceName)
+    return true
 end
 --------------------------------------------------------------------------------
 return 
@@ -70,60 +54,71 @@ return
         --LogDebug(domoticz, "--==[ Start ]==-----------------------------------")
 
         -- Execute perl script
-        cmd = string.format("perl %s --numeric --meter=0 --port=%d %s", script, port, ip) 
+        cmd = string.format("perl %s --debug --numeric --meter=0 --port=%d %s", script, port, ip) 
         local f = io.popen(cmd, 'r')
-        local result = f:read('*a')
+        local csv = {}
+        for line in f:lines() do
+            table.insert(csv, line)
+        end
         f:close()
+        
+        if #csv < 2 then
+            LogError(domoticz, "No valid response. Expected two lines.")
+            return
+        end
 
-        -- Convert CSV result line into array
-        local fields = {}
-        for value in result:gmatch('([^,]+)') do
-            table.insert(fields, tonumber(value))
+        --Parse header
+        --timestamp,status,ac_power,dc_power,total_production,ac_voltage,ac_current,dc_voltage,dc_current,temperature,exported_energy_m1,imported_energy_m1,exported_energy_m2,imported_energy_m2
+        local names = {}
+        for value in csv[1]:gmatch('([^,]+)') do
+            table.insert(names, value)
         end
         
-        -- Update domoticz counters
-        local deviceExists = domoticz.utils.deviceExists --Caching for speed
-        if #fields > 10 then
-            if deviceExists("SE ModBus Energy") then
-                local dzDevice = domoticz.devices("SE ModBus Energy")
-                dzDevice.updateElectricity(fields[ac_power], fields[total_production])
-            end
-            if deviceExists("SE ModBus Current AC") then
-                local dzDevice = domoticz.devices("SE ModBus Current AC")
-                dzDevice.updateCurrent(fields[ac_current])
-    
-            end
-            if deviceExists("SE ModBus Current DC") then
-                local dzDevice = domoticz.devices("SE ModBus Current DC")
-                dzDevice.updateCurrent(fields[dc_current])
-            end
-            if deviceExists("SE ModBus Temperature") then
-                local dzDevice = domoticz.devices("SE ModBus Temperature")
-                dzDevice.updateTemperature(fields[temperature])
-            end
-            if deviceExists("SE ModBus Status") then
-                local dzDevice = domoticz.devices("SE ModBus Status") -- Text sensor
-                local statusId = fields[status]
-                local statusString  = string.format("%d %s", statusId, statusStringMap[statusId])
-                if dzDevice.text ~= statusString then
-                    dzDevice.updateText(statusString)
-                end
-            end
-            if deviceExists("P1 kWh") and deviceExists("Energy Consumed") then
-                local dzDeviceP1 = domoticz.devices("P1 kWh")
-                local dzDeviceHouseEnergy = domoticz.devices("Energy Consumed")
+        -- Convert CSV result line into table
+        local solaredge = {}
+        local fieldcount = 0
+        for value in csv[2]:gmatch('([^,]+)') do
+            fieldcount = fieldcount + 1
+            solaredge[names[fieldcount]] = tonumber(value)
+        end
+        
+        if fieldcount < 10 then
+            LogError(domoticz, "No valid response. Expected 10 or more fields.")
+            return
+        end
+        
+        local dzDevice = {}
+        if GetDevice(domoticz, "SE ModBus Energy", dzDevice) then
+            dzDevice.meter.updateElectricity(solaredge.ac_power, solaredge.total_production)
+        end
 
-                local houseUsageWatt = dzDeviceP1.usage + fields[ac_power] - dzDeviceP1.usageDelivered
-                local houseUsageKwh = dzDeviceP1.usage1 + dzDeviceP1.usage2 - dzDeviceP1.return1 - dzDeviceP1.return2 + fields[total_production]
-                houseUsageKwh = math.max(houseUsageKwh, dzDeviceHouseEnergy.usage) -- Guarding rounding erros in the wrong direction
+        if GetDevice(domoticz, "SE ModBus Current AC", dzDevice) then
+            dzDevice.meter.updateCurrent(solaredge.ac_current)
+        end
 
-                dzDeviceHouseEnergy.updateElectricity(houseUsageWatt, houseUsageKwh)
+        if GetDevice(domoticz, "SE ModBus Current DC", dzDevice) then
+            dzDevice.meter.updateCurrent(solaredge.dc_current)
+        end
+        
+        if GetDevice(domoticz, "SE ModBus Temperature", dzDevice) then
+            dzDevice.meter.updateTemperature(solaredge.temperature)
+        end
 
-                --LogDebug(domoticz, string.format("Usage1 %d  Usage2 %d  return1 %d  return2 %d  production %d", dzDeviceP1.usage1, dzDeviceP1.usage2, dzDeviceP1.return1, dzDeviceP1.return2, fields[total_production]))
-                --LogDebug(domoticz, string.format("houseUsageKwh %d", houseUsageKwh))
+        if GetDevice(domoticz, "SE ModBus Status", dzDevice) then
+            local statusId = solaredge.status
+            local statusString  = string.format("%d : %s", statusId, statusStringMap[statusId] or "Unknown")
+            if dzDevice.meter.text ~= statusString then
+                dzDevice.meter.updateText(statusString)
             end
-        else
-            LogError(domoticz, "No valid response")
+        end
+        
+        if GetDevice(domoticz, "Energy Consumed", dzDevice) then
+            P1 = domoticz.devices("P1 kWh")
+            
+            local houseUsageWatt = P1.usage + solaredge.ac_power - P1.usageDelivered
+            local houseUsageKwh = P1.usage1 + P1.usage2 - P1.return1 - P1.return2 + solaredge.total_production
+            houseUsageKwh = math.max(houseUsageKwh, dzDevice.meter.usage) -- Guarding rounding erros in the wrong direction
+            dzDevice.meter.updateElectricity(houseUsageWatt, houseUsageKwh)
         end
 
         --LogDebug(domoticz, "--==[ End ]==-----------------------------------")
